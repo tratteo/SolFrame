33ba34ae5a1c7a8fbb5e53510775eed0aa8bd200
Matteo Beltrame <bltmtt@gmail.com>
2022-05-11 23:07:22 +0200

    Credits page, SolanaEndpointManager (#1)

---
+++ Runtime/Core/SolanaEndpointManager.cs
@@ -0,0 +1,35 @@
+﻿using Solnet.Rpc;
+using UnityEngine;
+
+namespace SolFrame
+{
+    public class SolanaEndpointManager : MonoBehaviour
+    {
+        [SerializeField] private bool dontDestroyOnLoad = false;
+        [SerializeField] private Cluster cluster;
+        [SerializeField] private bool useCustomEndpoint = false;
+        [SerializeField] private string customEndpoint = null;
+        private IRpcClient rpcClient;
+
+        public SolanaEndpointManager Instance { get; private set; } = null;
+
+        public IRpcClient GetClient()
+        {
+            rpcClient ??= useCustomEndpoint ? ClientFactory.GetClient(customEndpoint) : ClientFactory.GetClient(cluster);
+            return rpcClient;
+        }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                if (dontDestroyOnLoad) DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
